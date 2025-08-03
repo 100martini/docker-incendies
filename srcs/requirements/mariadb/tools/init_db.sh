fe7dcc36e441a0abb8b551fee6520aa2156e2f51
100martini <50martini50smd@gmail.com>
2025-08-03 13:53:15 +0100

    fix mariadb

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -8,31 +8,32 @@ if [ -f /run/secrets/db_password ]; then
     MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 fi
 
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing database..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-fi
-
-mysqld --user=mysql --skip-networking &
-pid=$!
-
-echo "Waiting for MariaDB to start..."
-for i in $(seq 30); do
-    if mysqladmin ping -h localhost --silent; then
-        break
-    fi
-    sleep 1
-done
-
-mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    
+    mysqld --user=mysql --skip-networking &
+    pid=$!
+    
+    for i in $(seq 30); do
+        if mysqladmin ping --silent; then
+            break
+        fi
+        sleep 1
+    done
+    
+    mysql << EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+    
+    kill $pid
+    wait $pid
+fi
 
-kill $pid
-wait $pid
-
-exec mysqld --user=mysql --bind-address=0.0.0.0
+exec mysqld
