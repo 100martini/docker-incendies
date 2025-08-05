5d9db5477d8e1928fe190c162c0d97836a697c49
100martini <50martini50smd@gmail.com>
2025-08-05 22:04:46 +0100

    ftp-fix

--- srcs/requirements/bonus/ftp/tools/setup_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -4,12 +4,11 @@ if [ -f /run/secrets/ftp_password ]; then
     FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 fi
 
-adduser -D -h /home/$FTP_USER/ftp -s /bin/sh $FTP_USER
+adduser -D -h /home/$FTP_USER -s /bin/sh $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-mkdir -p /home/$FTP_USER/ftp/files
-chown -R $FTP_USER:$FTP_USER /home/$FTP_USER/ftp/files
-chmod a-w /home/$FTP_USER/ftp
+ln -s /var/www/html /home/$FTP_USER/wordpress
+chown -h $FTP_USER:$FTP_USER /home/$FTP_USER/wordpress
 
 mkdir -p /var/run/vsftpd/empty
 
