79f2842d7b47cd52436eb43f7703460bb1c4e5de
100martini <50martini50smd@gmail.com>
2025-08-05 17:07:09 +0100

    well.. fixed some issues

--- srcs/requirements/nginx/tools/generate_ssl.sh
+++ srcs/requirements/nginx/tools/generate_ssl.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+set -e
 
 if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
     echo "Generating SSL certificate for ${DOMAIN_NAME}..."
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout /etc/nginx/ssl/nginx.key \
         -out /etc/nginx/ssl/nginx.crt \
-        -subj "/C=MA/ST=Tangier/L=Tangier/O=42/OU=42/CN=${DOMAIN_NAME}"
+        -subj "/C=MA/ST=Benguerir/L=Benguerir/O=42/OU=1337/CN=${DOMAIN_NAME}"
 fi
+
+echo "Configuring nginx for domain: ${DOMAIN_NAME}"
+envsubst '${DOMAIN_NAME}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+nginx -t
 
 exec nginx -g "daemon off;"
